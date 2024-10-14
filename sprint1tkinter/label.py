ddeb441b9e5b004b1da0e2c798612d6276f7255a
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-14 17:20:31 +0200

    Terminada clase Label

--- sprint1tkinter/label.py
+++ sprint1tkinter/label.py
@@ -1,9 +1,23 @@
 import tkinter as tk
 
+def cambiar_texto():
+    label3.config(text="¡El texto ha cambiado!")
+
 ventana = tk.Tk()
-ventana.title("La ventana(the windows)")
+ventana.title("La ventana")
 ventana.geometry("300x150")
 
-etiqueta1=tk.Label(ventana, text="Güelcom pipol")
 
+label1 = tk.Label(ventana, text="Bienvenido a la aplicación")
+label1.pack(pady=5)
 
+label2 = tk.Label(ventana, text="Nicolás")
+label2.pack(pady=5)
+
+label3 = tk.Label(ventana, text="Haz clic en el botón para cambiar este texto")
+label3.pack(pady=5)
+
+boton = tk.Button(ventana, text="Cambiar texto", command=cambiar_texto)
+boton.pack(pady=5)
+
+ventana.mainloop()
