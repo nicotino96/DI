dc8373e82799f5f907df2733f342cc8d421b1de6
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-16 10:25:58 +0200

    canvas

--- sprint1tkinter/entry.py
+++ sprint1tkinter/entry.py
@@ -11,13 +11,13 @@ root.geometry("300x300")
 label=tk.Label(root,text="Escribe tu nombre: ")
 label.pack(pady=5)
 
-entrada= tk.Entry(root, width=30)
+entrada = tk.Entry(root, width=30)
 entrada.pack(pady=5)
 
 boton = tk.Button(root,text="Saludo",command=obtener_saludo)
 boton.pack(pady=5)
 
-label_resultado= tk.Label(root, text="")
+label_resultado = tk.Label(root, text="")
 label_resultado.pack(pady=5)
 
 root.mainloop()
