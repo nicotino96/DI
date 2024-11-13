93b38791acf334de09fee8e6630f8ef5b734bb28
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-13 09:41:40 +0100

    terminado resources.py

--- sprint3juegoTkinter/model.py
+++ sprint3juegoTkinter/model.py
@@ -6,7 +6,7 @@ class GameModel:
     def __init__(self, difficulty, player_name, cell_size=100):
         self.difficulty = difficulty
         self.player_name = player_name
-        self.cell_size = 100
+        self.cell_size = cell_size
     def _generate_board(self):
 
     def _load_images(self):
