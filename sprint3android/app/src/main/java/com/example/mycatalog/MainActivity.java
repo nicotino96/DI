57340abc65c7ad5ecc2b8dce3d77f7520a73edf4
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-21 12:28:30 +0100

    reiniciando tarea sprint android

--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -8,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CatalogActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_catalog);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        findViewById(R.id.btn_navigate_detail).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Navegar a DetailActivity
-                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
